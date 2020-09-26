121fdeadfd7d0e5a63f5489a25ce2aecb518eafa
katyvonkaty <kj0203@gmail.com>
2020-09-26 11:23:25 -0400

    updated

---
+++ src/components/Home.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import {connect} from "react-redux"
+import { addToCart } from './actions/cartActions'
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+
+// import { AccessAlarm, ThreeDRotation } from '@material-ui/icons';
+
+
+ class Home extends Component{
+
+    handleClick = (id)=>{
+        this.props.addToCart(id);
+    }
+
+    render(){
+        let itemList = this.props.items.map(item=>{
+            return(
+              <div class="row">
+                <div className="card col-sm-4" key={item.id}>
+                        <div className="card-image">
+                            <img src={item.img} alt={item.title}/>
+                            <span className="card-title">{item.title}</span>
+                            <span to="/" className="btn-floating halfway-fab waves-effect waves-light" onClick={()=>{this.handleClick(item.id)}}><AddCircleIcon /></span>
+                        </div>
+
+                        <div className="card-content">
+                            <p>{item.desc}</p>
+                            <p><b>Price: {item.price}$</b></p>
+                        </div>
+                 </div>
+                 </div>
+
+            )
+        })
+
+        return(
+            <div className="container">
+                <h3 className="center">Our items</h3>
+                <div className="box">
+                    {itemList}
+                </div>
+            </div>
+        )
+    }
+}
+const mapStateToProps = (state)=>{
+    return {
+      items: state.items
+    }
+  }
+const mapDispatchToProps= (dispatch)=>{
+
+    return{
+        addToCart: (id)=>{dispatch(addToCart(id))}
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Home)
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link} from "react-router-dom";
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+
+const Navbar = ()=>{
+   return(
+
+           <nav className="nav-wrapper">
+               <div className="container">
+                   <Link to="/" className="brand-logo">Shopping</Link>
+
+                   <ul className="right">
+                       <li><Link to="/">Shop</Link></li>
+                       <li><Link to="/cart">My cart</Link></li>
+                       <li><Link to="/cart">Shopping Cart <ShoppingCartIcon /></Link></li>
+                   </ul>
+               </div>
+           </nav>
+
+   )
+}
+
+export default Navbar;
