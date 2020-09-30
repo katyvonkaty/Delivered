3e21cfc4489397d7d8c69e824ef69444df3442f8
katyvonkaty <kj0203@gmail.com>
2020-09-30 19:27:24 -0400

    updated

--- src/index.js
+++ src/index.js
@@ -8,22 +8,10 @@ import "../node_modules/materialize-css/dist/js/materialize.min.js";
 import cartReducer from './components/reducers/cartReducer';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-// const path = require("path");
-// const express = require("express");
-// const app = express();
-// const publicPath = path.join(__dirname, '..', "public") ;
-// const port = process.env.PORT || 3000;
 
-//
-// app.use(express.static(publicPath)); //here is important thing - no static directory, because all static :)
-//
-// app.get("/*", function(req, res) {
-//   res.sendFile(path.join(publicPath, "index.html"));
-// });
 
 const store = createStore(cartReducer);
 
 
 
-// ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
