c32d695aeaa2418048146b1521e2371e177c9cc0
katyvonkaty <kj0203@gmail.com>
2020-09-30 19:02:18 -0400

    updated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 import "../node_modules/materialize-css/dist/css/materialize.min.css";
@@ -9,6 +10,10 @@ import cartReducer from './components/reducers/cartReducer';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
+
+
 const store = createStore(cartReducer);
+
+
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
