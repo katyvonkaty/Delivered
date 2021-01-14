ffad6672e32b8ba8ed07af679714a5c7313a619b
katyvonkaty <kj0203@gmail.com>
2021-01-14 14:31:11 -0500

    updated

--- src/components/NavbarItems.js
+++ src/components/NavbarItems.js
@@ -1,28 +1,65 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+//
+// import { Link} from "react-router-dom";
+// import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 
-import { Link} from "react-router-dom";
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 
+import React, { Component } from 'react'
+import { Input, Menu } from 'semantic-ui-react'
 
+ class NavbarItems extends Component {
+  state = { activeItem: 'home' }
 
-class NavbarItems extends Component{
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
   render() {
-    return(
-      <nav className="nav-wrapper" centered>
-          <div className="container">
-            <ul className="center aligned">
-                <li><Link to="/about"> Shipping </Link></li>
-                <li><Link to="/sweets">Sweets</Link></li>
-                <li><Link to="/cart">Shopping Cart</Link></li>
-                <li><Link to="/about"> Shipping </Link></li>
-                <li><Link to="/sweets">Sweets</Link></li>
-                <li><Link to="/cart">Shopping Cart <ShoppingCartIcon /></Link></li>
-            </ul>
-          </div>
-      </nav>
+    const { activeItem } = this.state
+
+    return (
+      <Menu.Menu position='center'>
+      <Menu secondary >
+        <Menu.Item
+          name='home'
+           position='center'
+          active={activeItem === 'home'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name='messages'
+          active={activeItem === 'messages'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name='friends'
+          active={activeItem === 'friends'}
+          onClick={this.handleItemClick}
+        />
+        </Menu>
+        </Menu.Menu>
     )
   }
 }
+
+
+// class NavbarItems extends Component{
+//   render() {
+//     return(
+//       <Grid centered>
+//       <nav className="nav-wrapper" centered>
+//           <div className="container">
+//             <ul className="center aligned">
+//                 <li><Link to="/about"> Shipping </Link></li>
+//                 <li><Link to="/sweets">Sweets</Link></li>
+//                 <li><Link to="/cart">Shopping Cart</Link></li>
+//                 <li><Link to="/about"> Shipping </Link></li>
+//                 <li><Link to="/sweets">Sweets</Link></li>
+//                 <li><Link to="/cart">Shopping Cart <ShoppingCartIcon /></Link></li>
+//             </ul>
+//           </div>
+//       </nav>
+//     )
+//   }
+// }
 
 
 
